3610905754c916a6a298471265eb469b23b8da5a
Navid Vakili <vakili.jd@gmail.com>
2021-06-20 09:44:45 +0430

    fix error config file load2

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ header("Access-Control-Allow-Methods: POST");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-require_once __DIR__ . '/../Config.php';
+require_once __DIR__ . '/Config.php';
 
 use Gregwar\Captcha\CaptchaBuilder;
 
